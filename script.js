d4f3227f0a1ef369d7416a37651ac84d07d436dd
joninet2971 <j.desplats@itecriocuarto.org.ar>
2024-07-24 21:22:47 -0300

    corrijo menu

--- script.js
+++ script.js
@@ -55,12 +55,3 @@ document.addEventListener('DOMContentLoaded', function() {
 
     startAutoSlide();
 });
-
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
-    }
-});
