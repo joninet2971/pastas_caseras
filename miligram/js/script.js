986682c1ba57d4ffe8f9fbc4e82fb88f5da46c71
octavio <octavionvictorio@gmail.com>
2024-08-19 19:12:47 -0300

    footer

--- miligram/js/script.js
+++ miligram/js/script.js
@@ -1,7 +1,11 @@
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    const menu = document.querySelector('.menu');
-    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
-  });
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
+    }
+});
 
 document.addEventListener('DOMContentLoaded', function() {
     const prevButton = document.querySelector('.prev');
